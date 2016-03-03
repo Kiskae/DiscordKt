b2413ef564d5b9e8abd5fa75930778830b128b06
Kiskae <kiskae@serverpeon.net>
2016-03-03 22:37:09 +0100

    Add current regions as a static resource

--- api/src/main/kotlin/net/serverpeon/discord/model/Region.kt
+++ api/src/main/kotlin/net/serverpeon/discord/model/Region.kt
@@ -23,4 +23,21 @@ interface Region {
         AFRICA, // Currently unused
         UNKNOWN
     }
+
+    /**
+     * Known regions, might change at some point in the future.
+     *
+     * Use [ClientModel.getAvailableServerRegions] to get an up-to-date list.
+     */
+    enum class KnownRegions(override val id: String, override val continent: Continent) : Region {
+        AMSTERDAM("amsterdam", Continent.EUROPE),
+        LONDON("london", Continent.EUROPE),
+        SINGAPORE("singapore", Continent.ASIA),
+        FRANKFURT("frankfurt", Continent.EUROPE),
+        US_EAST("us-east", Continent.NORTH_AMERICA),
+        US_CENTRAL("us-central", Continent.NORTH_AMERICA),
+        US_SOUTH("us-south", Continent.NORTH_AMERICA),
+        US_WEST("us-west", Continent.NORTH_AMERICA),
+        SYDNEY("sydney", Continent.AUSTRALIA)
+    }
 }
